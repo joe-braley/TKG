db5ad6d20117de8f214703b39bf403d83c1ee6e3
joe-braley <joebraley@microsoft.com>
2024-01-16 12:05:34 -0800

    re-enabling logging

--- src/org/openj9/envInfo/CmdExecutor.java
+++ src/org/openj9/envInfo/CmdExecutor.java
@@ -1,27 +1,30 @@
 /*******************************************************************************
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 package org.openj9.envInfo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Duration;
 import java.util.Arrays;
-import java.util.Map;
+import java.util.concurrent.*;
 
 public class CmdExecutor {
 	private static CmdExecutor instance;
+
+	private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(15);
 
 	private CmdExecutor() {
 	}
@@ -34,30 +37,53 @@ public class CmdExecutor {
 	}
 
 	public String execute(String[] commands) {
-		String rt = null;
+		return execute(commands, DEFAULT_TIMEOUT);
+	}
+
+	public String execute(String[] commands, Duration timeout) {
+		Process proc = null;
+
+		ExecutorService executor = Executors.newSingleThreadExecutor();
 		try {
 			ProcessBuilder builder = new ProcessBuilder(Arrays.asList(commands));
-			Map<String, String> env = builder.environment();
-			// clear env and copy from the parent shell environment
-			env.clear();
-			Map<String, String> shellEnv = System.getenv();
-			env.putAll(shellEnv);
 			builder.redirectErrorStream(true);
-			Process proc = builder.start();
-			BufferedReader stdOutput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+			proc = builder.start();
 
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-			String newline = "";  
-			while ((line = stdOutput.readLine()) != null) {
-				sb.append(newline).append(line);
-				newline = "\n";
+			Process finalProc = proc;
+			CompletableFuture<String> output = CompletableFuture.supplyAsync(() -> {
+				String rt;
+				try {
+					BufferedReader stdOutput = new BufferedReader(new InputStreamReader(finalProc.getInputStream()));
+					StringBuilder sb = new StringBuilder();
+					String line;
+					String newline = "";
+					while ((line = stdOutput.readLine()) != null) {
+						sb.append(newline).append(line);
+						newline = "\n";
+					}
+					rt = sb.toString();
+					finalProc.waitFor();
+					return rt;
+				} catch (IOException | InterruptedException e) {
+					return "Command could not be executed " + String.join(" ", Arrays.asList(commands));
+				}
+			}, executor);
+
+			return output.get(timeout.toMillis(), TimeUnit.MILLISECONDS);
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			String message = "Process failed to exit: " + String.join(" ", Arrays.asList(commands));
+			System.err.println(message);
+			return message;
+		} catch (IOException e) {
+			String message = "Process failed to run: " + String.join(" ", Arrays.asList(commands));
+			System.err.println(message);
+			return message;
+		} finally {
+			if (proc != null && proc.isAlive()) {
+				System.err.println("Forcibly stopping process " + String.join(" ", Arrays.asList(commands)));
+				proc.destroyForcibly();
 			}
-			rt = sb.toString();
-			proc.waitFor();
-		} catch (IOException | InterruptedException e) {
-			return "Command could not be executed";
+			executor.shutdownNow();
 		}
-		return rt;
 	}
 }
